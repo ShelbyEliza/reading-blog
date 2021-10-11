e08eacc20bee1e1a8c4becf98ce13ac48679c062
Shelby Shipley <shelbyelizashipley@gmail.com>
2021-10-11 15:17:41 -0500

    added head, nav, footer partials. created more view pages. required ejs and set it as view engine.

--- app.js
+++ app.js
@@ -1,11 +1,30 @@
 const express = require("express");
+const { render } = require("ejs");
 
 const app = express();
+
+app.set("view engine", "ejs");
 
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.sendFile(`${__dirname}/views/index.html`);
+  res.render("index", { title: "Reading Blog" });
+});
+
+app.get("/new-blog", (req, res) => {
+  res.render("new-blog", { title: "New Blog" });
+});
+
+app.get("/view-blog", (req, res) => {
+  res.render("view-blog", { title: "View Blog" });
+});
+
+app.get("/search", (req, res) => {
+  res.render("search", { title: "Search for a Post" });
+});
+
+app.get("/view-quote", (req, res) => {
+  res.render("view-quote", { title: "View Quote" });
 });
 
 app.listen(3000);
